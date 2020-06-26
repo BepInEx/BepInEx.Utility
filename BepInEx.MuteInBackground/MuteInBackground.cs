75c1a4e429252ca27e2c2003fdac0146d133e593
DeathWeasel1337 <deathweasel1337@gmail.com>
2020-06-26 14:55:42 -0500

    Add MuteInBackground

---
+++ BepInEx.MuteInBackground/MuteInBackground.cs
@@ -0,0 +1,46 @@
+﻿using BepInEx.Configuration;
+using BepInEx.Logging;
+using UnityEngine;
+
+namespace BepInEx
+{
+    /// <summary>
+    /// Mute the game in background
+    /// </summary>
+    [BepInPlugin(GUID, PluginName, Version)]
+    public partial class MuteInBackground : BaseUnityPlugin
+    {
+        public const string GUID = "com.deathweasel.bepinex.muteinbackground";
+        public const string PluginName = "Mute In Background";
+        public const string PluginNameFull = "BepInEx.MuteInBackground";
+        public const string Version = "1.0";
+        internal static new ManualLogSource Logger;
+
+        internal static float? OriginalVolume = null;
+
+        public static ConfigEntry<bool> ConfigMuteInBackground { get; private set; }
+
+        internal void Start()
+        {
+            Logger = base.Logger;
+            ConfigMuteInBackground = Config.Bind("Config", "Mute In Background", false, "Whether to mute the game when in the background, i.e. alt-tabbed.");
+        }
+
+        internal void OnApplicationFocus(bool hasFocus)
+        {
+            if (hasFocus)
+            {
+                //Restore the original volume if one was previously set
+                if (OriginalVolume != null)
+                    AudioListener.volume = (float)OriginalVolume;
+                OriginalVolume = null;
+            }
+            else if (ConfigMuteInBackground.Value)
+            {
+                //Store the original volume and set the volume to zero
+                OriginalVolume = AudioListener.volume;
+                AudioListener.volume = 0;
+            }
+        }
+    }
+}
---
+++ BepInEx.MuteInBackground/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using static BepInEx.MuteInBackground;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright ©DeathWeasel1337 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyTitle(PluginNameFull)]
+[assembly: AssemblyProduct(PluginNameFull)]
+[assembly: AssemblyDescription(PluginName)]
+[assembly: AssemblyVersion(Version)]
+[assembly: AssemblyFileVersion(Version)]
