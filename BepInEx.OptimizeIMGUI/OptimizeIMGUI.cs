1966cc8f99665d2a59e3f953e38635ddc5f99f88
ManlyMarco <dummyaccx@gmail.com>
2020-09-20 02:01:45 +0200

    Added GUILayoutOption caching

--- BepInEx.OptimizeIMGUI/OptimizeIMGUI.cs
+++ BepInEx.OptimizeIMGUI/OptimizeIMGUI.cs
@@ -2,9 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Reflection.Emit;
-using System.Text;
-using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
 using UnityEngine;
@@ -116,6 +115,92 @@ namespace BepInEx
                 if (c != 3) throw new InvalidOperationException("IL footprint does not match what was expected");
                 return replacementInstr;
             }
+
+            #region Cache
+
+            private static readonly Dictionary<int, Dictionary<float, GUILayoutOption>> _layoutOptionCache =
+                new Dictionary<int, Dictionary<float, GUILayoutOption>>();
+
+            private static readonly ConstructorInfo _layoutOptionConstructor;
+            private static readonly GUILayoutOption _widthTrue;
+            private static readonly GUILayoutOption _widthFalse;
+            private static readonly GUILayoutOption _heightTrue;
+            private static readonly GUILayoutOption _heightFalse;
+
+            private static GUILayoutOption CreateNewGuiLayoutOption(int type, object value)
+            {
+                return (GUILayoutOption)_layoutOptionConstructor.Invoke(new[] { type, value });
+            }
+
+            static Hooks()
+            {
+                _layoutOptionConstructor = AccessTools.FirstConstructor(typeof(GUILayoutOption), info => info.GetParameters().Length == 2);
+                _widthTrue = CreateNewGuiLayoutOption(6, 1);
+                _widthFalse = CreateNewGuiLayoutOption(6, 0);
+                _heightTrue = CreateNewGuiLayoutOption(7, 1);
+                _heightFalse = CreateNewGuiLayoutOption(7, 0);
+            }
+
+            [HarmonyTranspiler]
+            [HarmonyPatch(typeof(GUILayout), nameof(GUILayout.Width))]
+            [HarmonyPatch(typeof(GUILayout), nameof(GUILayout.MinWidth))]
+            [HarmonyPatch(typeof(GUILayout), nameof(GUILayout.MaxWidth))]
+            [HarmonyPatch(typeof(GUILayout), nameof(GUILayout.Height))]
+            [HarmonyPatch(typeof(GUILayout), nameof(GUILayout.MinHeight))]
+            [HarmonyPatch(typeof(GUILayout), nameof(GUILayout.MaxHeight))]
+            private static IEnumerable<CodeInstruction> CachedGetOptionFPatch(IEnumerable<CodeInstruction> instructions)
+            {
+                return new CodeMatcher(instructions)
+                    .MatchForward(false,
+                        new CodeMatch(OpCodes.Box),
+                        new CodeMatch(OpCodes.Newobj))
+                    .SetAndAdvance(OpCodes.Nop, null)
+                    .Set(OpCodes.Call, AccessTools.Method(typeof(Hooks), nameof(CachedGetOptionF)))
+                    .Instructions();
+            }
+
+            private static GUILayoutOption CachedGetOptionF(int type, float value)
+            {
+                if (!_layoutOptionCache.TryGetValue(type, out var cache))
+                {
+                    cache = new Dictionary<float, GUILayoutOption>();
+                    _layoutOptionCache[type] = cache;
+                }
+
+                if (!cache.TryGetValue(value, out var result))
+                {
+                    result = CreateNewGuiLayoutOption(type, value);
+                    cache[value] = result;
+                }
+
+                return result;
+            }
+
+            [HarmonyTranspiler]
+            [HarmonyPatch(typeof(GUILayout), nameof(GUILayout.ExpandWidth))]
+            [HarmonyPatch(typeof(GUILayout), nameof(GUILayout.ExpandHeight))]
+            private static IEnumerable<CodeInstruction> CachedGetOptionBPatch(IEnumerable<CodeInstruction> instructions)
+            {
+                return new CodeMatcher(instructions)
+                    .MatchForward(false,
+                        new CodeMatch(OpCodes.Box),
+                        new CodeMatch(OpCodes.Newobj))
+                    .SetAndAdvance(OpCodes.Nop, null)
+                    .Set(OpCodes.Call, AccessTools.Method(typeof(Hooks), nameof(CachedGetOptionB)))
+                    .Instructions();
+            }
+
+            private static GUILayoutOption CachedGetOptionB(int type, int value)
+            {
+                if (type == 6)
+                    return value == 0 ? _widthFalse : _widthTrue;
+                if (type == 7)
+                    return value == 0 ? _heightFalse : _heightTrue;
+
+                throw new ArgumentException("Unknown type " + type, nameof(type));
+            }
+
+            #endregion
         }
     }
 }
