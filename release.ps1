4ea0583c8508c498b9d3cbe2cb1c62aa2ea19d77
ManlyMarco <dummyaccx@gmail.com>
2021-05-12 12:47:39 +0200

    Change build output paths to account for patchers

--- release.ps1
+++ release.ps1
@@ -4,11 +4,10 @@ $copy = $dir + "/copy/BepInEx"
 Remove-Item -Force -Path ($dir + "/copy") -Recurse -ErrorAction SilentlyContinue
 Remove-Item -Force -Path ($dir + "/out") -Recurse -ErrorAction SilentlyContinue
 New-Item -ItemType Directory -Force -Path ($copy + "/plugins")
-New-Item -ItemType Directory -Force -Path ($dir + "/out")
 
-foreach ($filepath in [System.IO.Directory]::EnumerateFiles($dir,"*.dll"))
+foreach ($filepath in [System.IO.Directory]::EnumerateFiles($dir + "/BepInEx/plugins","*.dll"))
 {
-	$filename = $filepath.Replace($dir, "")
+	$filename = Split-Path $filepath -leaf
 
 	Remove-Item -Force -Path ($copy) -Recurse
 	New-Item -ItemType Directory -Force -Path ($copy + "/plugins/")
@@ -18,7 +17,22 @@ foreach ($filepath in [System.IO.Directory]::EnumerateFiles($dir,"*.dll"))
 	$zipfilename = $filename.Replace(".dll", " " + $version + ".zip")
 
 	"Creating archive: " + $zipfilename
-	Compress-Archive -Path $copy -Force -CompressionLevel "Optimal" -DestinationPath ($dir + "out/" + $zipfilename)
+	Compress-Archive -Path $copy -Force -CompressionLevel "Optimal" -DestinationPath ($dir + $zipfilename)
+}
+
+foreach ($filepath in [System.IO.Directory]::EnumerateFiles($dir + "/BepInEx/patchers","*.dll"))
+{
+	$filename = Split-Path $filepath -leaf
+
+	Remove-Item -Force -Path ($copy) -Recurse
+	New-Item -ItemType Directory -Force -Path ($copy + "/patchers/")
+	Copy-Item -Path ($filepath) -Destination ($copy + "/patchers/") -Force
+
+	$version = "v" + (Get-ChildItem -Path ($filepath) -Filter "*.dll" -Force)[0].VersionInfo.FileVersion.ToString()
+	$zipfilename = $filename.Replace(".dll", " " + $version + ".zip")
+
+	"Creating archive: " + $zipfilename
+	Compress-Archive -Path $copy -Force -CompressionLevel "Optimal" -DestinationPath ($dir + $zipfilename)
 }
 
 Remove-Item -Force -Path ($dir + "/copy") -Recurse
