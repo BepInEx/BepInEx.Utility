537600ed4f1f04ab4f476ddad84bcc2dd971d523
hanabi1224 <harlowmoo@gmail.com>
2021-07-21 19:59:18 +0800

    normalize assembly info

--- BepInEx.EnableResize/Properties/AssemblyInfo.cs
+++ BepInEx.EnableResize/Properties/AssemblyInfo.cs
@@ -2,35 +2,14 @@
 using System.Runtime.InteropServices;
 using static BepInEx.EnableResize;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("BepInEx.EnableResize")]
-[assembly: AssemblyDescription(PluginName)]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("https://github.com/BepInEx/BepInEx.Utility")]
-[assembly: AssemblyProduct("BepInEx.EnableResize")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b8cfc3bf-4724-4c80-86ac-6aa22af2fe3b")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyTitle(GUID)]
+[assembly: AssemblyProduct(GUID)]
+[assembly: AssemblyDescription(PluginName)]
 [assembly: AssemblyVersion(Version)]
 [assembly: AssemblyFileVersion(Version)]
