f5f5357a7d86cc975516f964d04c7bfe9ed80abe
hanabi1224 <harlowmoo@gmail.com>
2021-07-21 19:34:29 +0800

    use net35 / restore assemblyinfo

---
+++ BepInEx.MuteInBackground/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using static BepInEx.MuteInBackground;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("https://github.com/BepInEx/BepInEx.Utility")]
+[assembly: AssemblyCopyright("Copyright ©DeathWeasel1337 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyTitle(GUID)]
+[assembly: AssemblyProduct(GUID)]
+[assembly: AssemblyDescription(PluginName)]
+[assembly: AssemblyVersion(Version)]
+[assembly: AssemblyFileVersion(Version)]
